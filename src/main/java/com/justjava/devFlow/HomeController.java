8dcbe11efe9c0187ebe2ab488d6d22f2a6c5a9a7
Akinkunmi <akinrindeakinkunmi2006@gmail.com>
2025-09-18 12:24:43 +0100

    Dashboard made dynamic

--- src/main/java/com/justjava/devFlow/HomeController.java
+++ src/main/java/com/justjava/devFlow/HomeController.java
@@ -1,14 +1,48 @@
 package com.justjava.devFlow;
 
+import org.flowable.engine.HistoryService;
+import org.flowable.engine.RuntimeService;
+import org.flowable.engine.history.HistoricProcessInstance;
+import org.flowable.engine.runtime.ProcessInstance;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
+import java.util.List;
+
 @Controller
 public class HomeController {
+    @Autowired
+    RuntimeService runtimeService;
+
+    @Autowired
+    HistoryService historyService;
     @GetMapping("/")
     public String home(Model model) {
-        model.addAttribute("message", "Welcome to Thymeleaf + Spring Boot!");
+        List<ProcessInstance> projects = runtimeService
+                .createProcessInstanceQuery()
+                .processDefinitionKey("softwareEngineeringProcess")
+                .includeProcessVariables()
+                .active()
+                .list();
+        projects.forEach(project -> {
+            System.out.println(" The Process Instance" +
+                    " Here=ID=="+project.getProcessInstanceId()
+                    +" the start time ==="+project.getStartTime()
+                    +" the variables==="+project.getProcessVariables());
+        });
+        List<HistoricProcessInstance> completedProcess =historyService
+                .createHistoricProcessInstanceQuery()
+                .finished()
+                .orderByProcessInstanceEndTime()
+                .desc()
+                .list();
+        System.out.println(" The Completed Process Here==="+completedProcess.size());
+        model.addAttribute("projects",projects);
+        model.addAttribute("completedProject",completedProcess.size());
+        model.addAttribute("activeProject",projects.size());
+
         return "dashboard";
     }
 }
